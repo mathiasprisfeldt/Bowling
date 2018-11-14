fed8e849b9074553a282b861e41e64cbb280c0ae
Mathias Prisfeldt <contact@mathiasprisfeldt.me>
2018-11-14 16:47:21 +0100

    Test

--- src/test/java/dk/eaaa/BowlingFrameTest.java
+++ src/test/java/dk/eaaa/BowlingFrameTest.java
@@ -13,7 +13,7 @@ public class BowlingFrameTest {
 	public void initializeFrame() {
 		frame = new Frame();
 	}
-	
+
 	@Test
 	public void testGetScore() throws Exception {
 		assertEquals(0, frame.getScore());
@@ -22,39 +22,39 @@ public class BowlingFrameTest {
 		frame.rollSecond(1);
 		assertEquals(6, frame.getScore());
 	}
-	
+
 	@Test
 	public void testIsStrike() throws Exception {
-		frame.rollFirst(10);
+		frame.rollFirst(5);
 		assertTrue(frame.isStrike());
 	}
-	
+
 	@Test
 	public void testIsNotStrike() throws Exception {
 		assertFalse(frame.isStrike());
 	}
-	
+
 	@Test
 	public void testIsSpare() throws Exception {
 		frame.rollFirst(5);
 		frame.rollSecond(5);
 		assertTrue(frame.isSpare());
 	}
-	
+
 	@Test
 	public void testIsNotSpareItIsStrike() throws Exception {
 		frame.rollFirst(10);
 		assertFalse(frame.isSpare());
 		assertTrue(frame.isStrike());
 	}
-	
+
 	@Test
 	public void testIsNotSpare() throws Exception {
 		frame.rollFirst(6);
 		frame.rollSecond(3);
 		assertFalse(frame.isSpare());
 	}
-	
+
 	@Test(expected = Exception.class)
 	public void testInvalidNumberOfPinsNegativeNumbr() throws Exception {
 		frame.rollFirst(-1);
@@ -64,8 +64,8 @@ public class BowlingFrameTest {
 	public void testInvalidNumberOfPinsMoreThanTen() throws Exception {
 		frame.rollFirst(11);
 	}
-	
-	@Test(expected = Exception.class) 
+
+	@Test(expected = Exception.class)
 	public void testNoSecondRollAllowedAfterStrike() throws Exception {
 		frame.rollFirst(10);
 		frame.rollSecond(10);
